068389d07245bf62669d85e7d196dbaa2150643e
FakeDazon <44048306+FakeDazon@users.noreply.github.com>
2019-11-19 18:47:47 -0800

    import code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,147 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  TalonSRX wristMotor = new TalonSRX(3);
+  TalonSRX liftMotor = new TalonSRX(5);
+  TalonSRX backRightMotor = new TalonSRX(10);
+  TalonSRX backLeftMotor = new TalonSRX(8);
+  TalonSRX frontRightMotor = new TalonSRX(6);
+  TalonSRX frontLeftMotor = new TalonSRX(1);
+  Joystick jStick = new Joystick(0);
+  Joystick bStick = new Joystick(2);
+  
+
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    liftMotor.setInverted(true);
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  
+  public void teleopPeriodic() {
+    //SIDE TO SIDE
+    //while(jStick.getRawAxis(4) == 1){
+      backRightMotor.set(ControlMode.PercentOutput, ((jStick.getRawAxis(4) / 2) * -0.5) + ((jStick.getRawAxis(1) / 2) * -0.45) );      //^  V    RFB
+      backLeftMotor.set(ControlMode.PercentOutput, ((jStick.getRawAxis(4) / 2) * 0.4) + ((jStick.getRawAxis(1) / 2) * -0.4));        //V  ^   RBF
+      frontRightMotor.set(ControlMode.PercentOutput, ((jStick.getRawAxis(4) / 2) * 0.4) + ((jStick.getRawAxis(1) / 2) * 0.4) );
+      frontLeftMotor.set(ControlMode.PercentOutput, ((jStick.getRawAxis(4) / 2) * 0.4) + ((jStick.getRawAxis(1) / 2) * -0.4));
+    //}
+    
+
+    /*  test code after open house
+    while(bStick.getRawButtonPressed(1)) {
+      liftMotor.set(ControlMode.PercentOutput, 0.3 );
+    }
+
+    while(bStick.getRawButtonPressed(2)){
+      liftMotor.set(ControlMode.PercentOutput, -0.3 )
+    }
+
+    if(bStick.getRawButtonPressed(3)){
+      liftMotor.set(ControlMode.PercentOutput, 0 );
+    }
+
+    */
+
+    if(bStick.getRawButtonPressed(1)){
+      liftMotor.set(ControlMode.PercentOutput, 0.5 );
+    }
+    if(bStick.getRawButtonPressed(2)){
+      liftMotor.set(ControlMode.PercentOutput, -0.5 );
+    }
+    if(bStick.getRawButtonPressed(3)){
+      liftMotor.set(ControlMode.PercentOutput, 0 );
+    }
+  }
+  
+
+  /**
+   * This function is called periodically during test mode.
+*/
+  @Override
+  public void testPeriodic() {
+  }
+}
